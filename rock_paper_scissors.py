1c91fc7fd8d5dcc012a4148eb7eac333c630025b
Atanas Andonov <andonov.atanas98@gmail.com>
2025-05-25 15:46:25 +0300

    Added restart the game function.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -12,38 +12,46 @@ def print_in_blue(result):
 def print_in_grey(result):
     print(f"\033[90m {result}\033[00m")
 
-rock = "Rock"
-paper = "Paper"
-scissors = "Scissors"
+while True:
+    rock = "Rock"
+    paper = "Paper"
+    scissors = "Scissors"
 
-player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
+    player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
 
-if player_move == "r":
-    player_move = rock
-elif player_move == "p":
-    player_move = paper
-elif player_move == "s":
-    player_move = scissors
-else:
-    raise SystemExit("Invalid Input. Try again...")
+    if player_move == "r":
+        player_move = rock
+    elif player_move == "p":
+        player_move = paper
+    elif player_move == "s":
+        player_move = scissors
+    else:
+        raise SystemExit("Invalid Input. Try again...")
 
-computer_random_number = random.randint(1, 3)
-computer_move = ""
+    computer_random_number = random.randint(1, 3)
+    computer_move = ""
 
-if computer_random_number == 1:
-    computer_move = rock
-elif computer_random_number == 2:
-    computer_move = paper
-elif computer_random_number == 3:
-    computer_move = scissors
+    if computer_random_number == 1:
+        computer_move = rock
+    elif computer_random_number == 2:
+        computer_move = paper
+    elif computer_random_number == 3:
+        computer_move = scissors
 
-print_in_blue(f"The computer chose {computer_move}.")
+    print_in_blue(f"The computer chose {computer_move}.")
 
-if (player_move == rock and computer_move == scissors) or \
-        (player_move == paper and computer_move == rock) or \
-        (player_move == scissors and computer_move == paper):
-    print_in_blue("You win!")
-elif player_move == computer_move:
-    print_in_grey("Draw!")
-else:
-    print_in_red("You lose!")
+    if (player_move == rock and computer_move == scissors) or \
+            (player_move == paper and computer_move == rock) or \
+            (player_move == scissors and computer_move == paper):
+        print_in_blue("You win!")
+    elif player_move == computer_move:
+        print_in_grey("Draw!")
+    else:
+        print_in_red("You lose!")
+
+    print()
+    print("Type [yes] to play again or [no] to quit: ")
+    play_again_response = input()
+
+    if not play_again_response == "yes":
+        break
