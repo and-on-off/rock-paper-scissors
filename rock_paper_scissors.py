3524bdf6e9cce2a2b958aa8cd08538139e8e0496
Atanas Andonov <andonov.atanas98@gmail.com>
2025-05-25 15:30:43 +0300

    Added colors to the console output.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,17 @@
 import random
 
+def print_in_red(result):
+    print(f"\033[91m {result}\033[00m")
+
+def print_in_green(result):
+    print(f"\033[92m {result}\033[00m")
+
+def print_in_blue(result):
+    print(f"\033[34m {result}\033[00m")
+
+def print_in_grey(result):
+    print(f"\033[90m {result}\033[00m")
+
 rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
@@ -28,8 +40,8 @@ elif computer_random_number == 3:
 if (player_move == rock and computer_move == scissors) or \
         (player_move == paper and computer_move == rock) or \
         (player_move == scissors and computer_move == paper):
-    print("You win!")
+    print_in_blue("You win!")
 elif player_move == computer_move:
-    print("Draw!")
+    print_in_grey("Draw!")
 else:
-    print("You lose!")
+    print_in_red("You lose!")
